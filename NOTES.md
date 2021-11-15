ab702d0a10a694dcfa82dbe1873a32aa9f0be3ac
Dominic Coman <dominic.coman@gmail.com>
2021-11-15 02:42:04 +0100

    Add Amount object
    Add Amount with tests
    Update NOTES.md to reflect decision made

--- NOTES.md
+++ NOTES.md
@@ -9,3 +9,19 @@ There are 3 user stories in the kata. We clearly need a BDD testing framework:
 Having some experience with the first one, I'll pick it. May try the others later. New Git branch: *jbehave*.\
 Add other useful but minimal dependencies like *Lombok*.\
 No persistence, minimal implementation => forget about *Spring* for now.
+
+##Domain
+All right. We have our jbehave framework, and we can see it works: `mvn integration-test`.\
+Our stories are just narratives for now. If we want frequent pushes and avoid pushing failing tests,
+we don't want to write scenarios and the BDD test classes right away: there would be too much code for the next push.\
+So just focus on the domain objects, and commit them one by one along their JUnit tests.
+
+- ###Amount
+A simple wrapper for a BigDecimal.
+
+This might be a major drawback with non-decimal currencies: https://en.wikipedia.org/wiki/Non-decimal_currency.
+What about 1 Galleon = 17 Sickles = 493 Knuts in Harry Potter?\
+
+The good news is that this special case is rare in real-world:
+only Mauritania and Madagascar still have non-decimal currencies -
+https://en-academic.com/dic.nsf/enwiki/658767.
---
+++ src/test/java/com/dominic/kata/bankaccount/domain/AmountTest.java
@@ -0,0 +1,122 @@
+package com.dominic.kata.bankaccount.domain;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(Enclosed.class)
+public class AmountTest {
+
+    @RunWith(Parameterized.class)
+    public static class SignTests {
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {Amount.of(42), Amount.of(-42)},
+                    {Amount.of(-42), Amount.of(42)},
+                    {Amount.of(0), Amount.of(0)}
+            });
+        }
+
+        private final Amount anAmount;
+        private final Amount expected;
+
+        public SignTests(Amount anAmount, Amount expected) {
+            this.anAmount = anAmount;
+            this.expected = expected;
+        }
+
+        @Test
+        public void
+        change_sign_should_work_correctly() {
+            assertThat(anAmount.changeSign(), is(equalTo(expected)));
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class AbsoluteValueTests {
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {Amount.of(42), Amount.of(42)},
+                    {Amount.of(-42L), Amount.of(42L)},
+                    {Amount.of(-42.0), Amount.of(42)},
+                    {Amount.of(0), Amount.of(0)}
+            });
+        }
+
+        private final Amount anAmount;
+        private final Amount expected;
+
+        public AbsoluteValueTests(Amount anAmount, Amount expected) {
+            this.anAmount = anAmount;
+            this.expected = expected;
+        }
+
+        @Test
+        public void
+        absolute_value_should_work_correctly() {
+            assertThat(anAmount.absoluteValue(), is(equalTo(expected)));
+        }
+    }
+
+    public static class NonParametrizedTests {
+        @Test
+        public void
+        should_be_equal_to_other_object_with_same_value() {
+            Amount firstFortyTwo = Amount.of(42);
+            Amount secondFortyTwo = Amount.of(42);
+
+            assertEquals(firstFortyTwo, secondFortyTwo);
+        }
+
+        @Test
+        public void
+        should_be_different_from_other_object_with_different_value() {
+            Amount fortyTwo = Amount.of(42);
+            Amount negativeFortyTwo = Amount.of(-42);
+
+            assertNotEquals(fortyTwo, negativeFortyTwo);
+        }
+
+        @Test
+        public void
+        adding_two_amounts_should_return_the_correct_sum() {
+            Amount one = Amount.of(1);
+            Amount two = Amount.of(2);
+
+            assertEquals(new BigDecimal(3), one.add(two).getValue());
+        }
+
+        @Test
+        public void
+        subtracting_two_amounts_should_return_the_correct_result() {
+            Amount one = Amount.of(1);
+            Amount two = Amount.of(2);
+
+            assertThat(new BigDecimal(-1), is(equalTo(one.subtract(two).getValue())));
+        }
+
+        @Test
+        public void
+        should_return_money_representation() {
+            Amount fortyTwo = Amount.of(42);
+
+            assertThat("42.00", is(equalTo(fortyTwo.moneyFormat())));
+        }
+    }
+
+}
